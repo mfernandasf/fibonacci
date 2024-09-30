1582037bd4d0e0baf4a17465f06a3369809ebcf5
Maria Fernanda <84775618+mfernandasf@users.noreply.github.com>
2024-09-30 13:11:57 -0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    if n <= 0:
+        print("Por favor, insira um número positivo.")
+    elif n == 1:
+        print("Sequência de Fibonacci até", n, "termo:")
+        print(0)
+    else:
+        print("Sequência de Fibonacci até", n, "termos:")
+        a, b = 0, 1
+        print(a, b, end=" ")
+
+        for i in range(2, n):
+            c = a + b
+            print(c, end=" ")
+            a, b = b, c
+        print()
+
+
+num_termos = int(input("Quantos termos da sequência de Fibonacci você quer? "))
+fibonacci(num_termos)
